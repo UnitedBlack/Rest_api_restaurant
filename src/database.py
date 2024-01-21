e03493b3ab4de8b84ce2a338390d3362fb32bb54
ShodanAI <91415015+UnitedBlack@users.noreply.github.com>
2024-01-22 02:19:52 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_URL = "postgresql://restaurant_admin:restaurant_password@localhost:5432/menus"
+SQLALCHEMY_URL = "postgresql://restaurant_admin:restaurant_password@db:5432/menus"
 
 engine = create_engine(SQLALCHEMY_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
